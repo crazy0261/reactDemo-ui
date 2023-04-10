7c25e65bf13e91e9c585160f99a8a298643a4813
menghuiding <dingmenghui026@163.com>
2023-04-11 07:51:36 +0800

    新增

--- src/components/1_setState/index.jsx
+++ src/components/1_setState/index.jsx
@@ -5,10 +5,16 @@ export default class Demo extends Component {
     state = {count:0}
 
     add =() =>{
-        // 获取count值
-        const {count} = this.state
-        // 更新状态
-        this.setState({count:count + 1})
+    //   // 对象式的state
+    //     // 获取count值
+    //     const {count} = this.state
+    //     // 更新状态
+    //     this.setState({count:count + 1},()=>{
+    //       console.log(this.state.count)
+    //     })
+
+          // 函数式的setState
+          this.setState( state => ({count:state.count +1}))
     }
 
   render() {
